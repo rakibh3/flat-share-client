0e849cb728223320fe2775f5731d508c6f437d53
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-06-08 02:31:57 +0600

    Select 4 images

--- src/app/(withDashboardLayout)/dashboard/(userDashboard)/_components/fileUpload.tsx
+++ src/app/(withDashboardLayout)/dashboard/(userDashboard)/_components/fileUpload.tsx
@@ -1,6 +1,45 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
 const FileUpload = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);
+
+  const handleFileChange = (event: any) => {
+    const files = Array.from(event.target.files as FileList);
+    const acceptedTypes = [
+      'image/svg+xml',
+      'image/png',
+      'image/jpeg',
+      'image/gif',
+    ];
+    let valid = true;
+
+    if (files.length !== 4) {
+      valid = false;
+      setErrorMessage('Please upload exactly 4 images.');
+      setSelectedImages([]);
+    } else {
+      for (let i = 0; i < files.length; i++) {
+        if (!acceptedTypes.includes(files[i].type)) {
+          valid = false;
+          setErrorMessage('Only SVG, PNG, JPG or GIF formats are supported.');
+          setSelectedImages([]);
+          break;
+        }
+      }
+    }
+
+    if (valid) {
+      setErrorMessage('');
+      setSelectedImages(files);
+      // Handle file upload logic here
+      console.log(files);
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex flex-col items-center justify-center w-full">
       <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
@@ -26,8 +65,32 @@ const FileUpload = () => {
             SVG, PNG, JPG or GIF (MAX. 800x400px)
           </p>
         </div>
-        <input id="dropzone-file" type="file" multiple className="hidden" />
+        <input
+          id="dropzone-file"
+          type="file"
+          multiple
+          className="hidden"
+          onChange={handleFileChange}
+          accept="image/svg+xml, image/png, image/jpeg, image/gif"
+        />
       </label>
+      <div>
+        {errorMessage && (
+          <p className="mt-2 text-sm text-red-600">{errorMessage}</p>
+        )}
+      </div>
+      <div className="mt-4 grid md:grid-cols-4 gap-4">
+        {selectedImages.map((file, index) => (
+          <div key={index} className="w-32 h-32">
+            <Image
+              src={URL.createObjectURL(file)}
+              alt={`Selected ${index}`}
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
