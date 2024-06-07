25d823e145d6dd98fbd3f9f212213479b49d6f92
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-06-07 20:58:35 +0600

    Complete HOMEPAGE

--- src/app/(withCommonLayout)/_components/Flat/FlatList.tsx
+++ src/app/(withCommonLayout)/_components/Flat/FlatList.tsx
@@ -55,7 +55,7 @@ const FlatList = ({ flats }: any) => {
               </div>
               <div>
                 <Link href={`/flat/${flat.id}`} className="block mt-4">
-                  <button className="bg-blue-500 text-white py-2 px-4 rounded-lg w-full">
+                  <button className="bg-rose-500 text-white py-2 px-4 rounded-lg w-full">
                     View Details
                   </button>
                 </Link>
